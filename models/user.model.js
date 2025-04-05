69185a9f1e2d0c682e6282b9174a9d651620bc6b
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-04-04 23:46:31 -0300

    fix: identing the code

--- models/user.model.js
+++ models/user.model.js
@@ -2,47 +2,51 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  appoiments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Appoiment",
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
+  ],
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: String,
-        required: true,
-    },
-    photo: {
-        type: String,
-        required: true,
-    },
-    appoiments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Appoiment",
-    }],
-    reviews: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Review",
-    }],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 //Hash antes de salvar
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
+  if (!this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
